aff47654e7d6b5cea29f5332e8a5514bdff20140
igornardin <igor.fdn@gmail.com>
2019-10-28 21:46:04 -0300

    Adjusted newton cotes function

--- app.py
+++ app.py
@@ -5,28 +5,49 @@ import math
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    a = request.args.get('a', default = 1, type = float)
-    b = request.args.get('b', default = 10, type = float)
-    n = request.args.get('n', default = 100, type = int)
-    h = float((b - a) / n)
-    sum = 0.5 * (f(a) + f(b))
-    for i in range (n):
-        x = a + (h * i)
-        sum = sum + f(x)
-    valor = sum * h
+    x0 = request.args.get('a', default=1.0, type=float)
+    x1 = request.args.get('b', default=10.0, type=float)
+    qtde_slices = request.args.get('n', default=100, type=int)
+    h_slice = (x1 - x0) / qtde_slices
+    _x1 = x0
+    result = 0
+    for i in range(qtde_slices):
+        _x0 = _x1
+        _x1 = _x0 + h_slice
+        fx0 = f(_x0)
+        fx1 = f(_x1)
+        if fx0 > fx1:
+            result = result + retangulo(h_slice, fx1)
+        else:
+            result = result + retangulo(h_slice, fx0)
+        if fx0 > fx1:
+            result = result + triangulo(h_slice, fx0 - fx1)           
+        elif fx0 < fx1:
+            result = result + triangulo(h_slice, fx1 - fx0)
+
     return jsonify(
         hostname=socket.gethostname(),
-        result=valor
+        result=result
     )
-#    html = "<h3>Hello {name}!</h3>" \
-#           "<b>Hostname:</b> {hostname}<br/>" \
-#           "<b>Result:</b> {result}<br/>"
-#    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), result=valor)
+
 
 def f(x):
-    return math.exp(- x * x / 2) / math.sqrt(2 * math.pi)
+    result = math.pow(x, 5) * 5
+    result = result + math.pow(x, 2)
+    result = result + x
+    return result
+
+
+def retangulo(base, altura):
+    return base * altura
+
+
+def triangulo(base, altura):
+    return (base * altura) / 2
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
